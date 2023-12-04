49bef6a9e6a5391e538fd1ab48d6260fe69a40eb
XnKTrial <shwethasenthil1002@gmail.com>
2023-12-04 12:21:50 +0530

    odd items color green

--- dom.js
+++ dom.js
@@ -1,9 +1,19 @@
 var header= document.querySelector('#main-header');
 header.style.borderBottom='solid 5px black';
+header.style.borderTop='solid 5px black';
+header.style.borderLeft='solid 5px black';
+header.style.borderRight='solid 5px black';
 
-var input=document.querySelector('#item2');
-input.style.backgroundColor='green';
+var titles=document.querySelectorAll('.title');
 
-var input=document.querySelector('#item3');
-input.style.visibility='hidden';
+var odd=document.querySelectorAll('li:nth-child(odd)')
 
+for(var i=0; i<odd.length;i++){
+    odd[i].style.backgroundColor='GREEN';
+}
+
+var even=document.querySelectorAll('li:nth-child(even)')
+
+for(var i=0; i<even.length;i++){
+    even[i].style.backgroundColor='yellow';
+}
