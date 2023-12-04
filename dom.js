da8d3b27130e5a8d4735856cb419dc06bc150b91
XnKTrial <shwethasenthil1002@gmail.com>
2023-12-04 11:13:13 +0530

    dom commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+//console.dir(document);
+
+var title=document.getElementById('main-header');
+    title.style.borderBottom='solid  5px black';
+    title.style.borderTop='solid  5px black';
+    title.style.borderLeft='solid  5px black';
+    title.style.fontWeight='bold';
+    title.style.color='yellow';
+
+
+var tit=document.getElementsByClassName('tit');
+tit[0].style.fontWeight = 'bold';
+tit[0].style.color='yellow';
+tit[0].style.backgroundColor='green';
