1c950dd5ee04372cc305c36ab5f8cbfa75cb3ed2
XnKTrial <shwethasenthil1002@gmail.com>
2023-12-05 16:20:35 +0530

    Complete

--- form.js
+++ form.js
@@ -3,15 +3,7 @@ var itemList = document.getElementById('items');
 var desc = document.getElementById('description');
 var filter = document.getElementById('filter');
 
-// Form submit event
-form.addEventListener('submit', addItem);
-form.addEventListener('submit', addDescription);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
 
-// Add item
 function addItem(e){
   e.preventDefault();
 
@@ -81,3 +73,18 @@ function removeItem(e){
 }
 
 // Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
